32f3954cd2be0059c865058a9ddd6e764680b48a
jhonnyer <jhonnyerg@unicauca.edu.co>
2022-11-25 18:06:07 -0500

    Proyecto 1

--- src/main/java/com/unab/app/controllers/IndexController.java
+++ src/main/java/com/unab/app/controllers/IndexController.java
@@ -1,7 +1,10 @@
 package com.unab.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/test")
 public class IndexController {
+
+//	private static final String USERNAME ="Jhonnyer";
+	@Value("${text.indexController.name}")
+	private String USERNAME;
 	
 	@RequestMapping(value="/prueba", method = RequestMethod.GET)
 	public String getPrueba() {
@@ -22,10 +29,18 @@ public class IndexController {
 		System.out.println("Prueba sin retorno");
 	}
 	
-	@GetMapping("/getObjeto")
+	@PostMapping("/getObjeto")
 	public Object getObjecto(@RequestBody Object object) {
 		System.out.println("Test Request Body");
 		System.out.println(object);
 		return object;
 	}
+	
+	@GetMapping("/indexParam/{nombre}")
+	public String indexParam(@PathVariable("nombre") String nombre) {
+		System.out.println(USERNAME);
+		System.out.println("Hola "+nombre+" bienvenido al sistema");
+		return nombre;
+	}
+	
 }

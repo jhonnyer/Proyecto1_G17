9386a7aa4e2a844c77845b80244561ee4a8069ad
jhonnyer <jhonnyerg@unicauca.edu.co>
2022-11-30 20:00:19 -0500

    Controller Proyecto G17

--- src/main/java/com/unab/app/controllers/DetalleFacturaController.java
+++ src/main/java/com/unab/app/controllers/DetalleFacturaController.java
@@ -1,10 +1,38 @@
 package com.unab.app.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value="/detalleFactura")
-public class DetalleFacturaController {
+import com.unab.app.models.DetalleFactura;
+import com.unab.app.service.interfaces.IDetalleFacturaService;
 
+@RestController
+@RequestMapping(value = "/detalleFactura")
+public class DetalleFacturaController {
+	@Autowired
+	private IDetalleFacturaService detalleFacturaService;
+
+	@PostMapping("/save")
+	public DetalleFactura save(@RequestBody DetalleFactura detalleFactura) {
+		detalleFacturaService.save(detalleFactura);
+		return detalleFactura;
+	}
+
+	@GetMapping("/getDetail/{idFactura}/{idProducto}")
+	public List<DetalleFactura> findByIDFacturaAndByIdProducto(@PathVariable("idFactura") Long idFactura,
+			@PathVariable("idProducto") Long idProducto) {
+		return detalleFacturaService.findByIDFacturaAndByIdProducto(idFactura, idProducto);
+	}
+
+	@GetMapping("/getDetail/{idFactura}")
+	public List<DetalleFactura> findByIDFactura(@PathVariable("idFactura") Long idFactura) {
+		return detalleFacturaService.findByIDFactura(idFactura);
+	}
 }
--- src/main/java/com/unab/app/controllers/FacturaController.java
+++ src/main/java/com/unab/app/controllers/FacturaController.java
@@ -1,10 +1,42 @@
 package com.unab.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unab.app.models.Factura;
+import com.unab.app.service.interfaces.IFacturaService;
+
 @RestController
-@RequestMapping(value="/factura")
+@RequestMapping(value = "/factura")
 public class FacturaController {
+	@Autowired
+	private IFacturaService facturaService;
+
+	@PostMapping("/save")
+	public Factura save(@RequestBody Factura factura) {
+		facturaService.save(factura);
+		return factura;
+	}
+
+	@GetMapping("/getFacturaId/{id}")
+	public Factura findFacturaById(@PathVariable("id") Long id) {
+		return facturaService.findFacturaById(id);
+	}
+
+	@GetMapping("/deleteFactura/{id}")
+	public void deleteFactura(@PathVariable("id") Long id) {
+		facturaService.deleteFactura(id);
+	}
+
+	@GetMapping("/deleteFactura/{idFactura}/{idProducto}")
+	public void updateFactura(@PathVariable("idFactura") Long ididFactura,
+			@PathVariable("idProducto") Long idProducto) {
+		facturaService.updateFactura(ididFactura, idProducto);
+	}
 
 }
